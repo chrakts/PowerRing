811dd54e54c56b8e4e16b7394ad3592fccb4bc6a
Christof <chrak@cmja.de>
2021-03-21 19:37:33 +0100

    Farblogik geändert

--- powerCalc.cpp
+++ powerCalc.cpp
@@ -1,6 +1,6 @@
 #include "powerCalc.h"
 
-#define BRIGHTNESS 6
+#define BRIGHTNESS 12
 
 volatile double powerGrid=3000,powerBatterie=650,powerSolar=4500;
 uint8_t powerLeds[NUMLEDS];
@@ -9,7 +9,33 @@ void calcLedsFromPower(void)
 {
 uint8_t posMarker=0,negMarker=NUMLEDS;
 int8_t temp;
+double eigenVerbrauch;
   fill_led_color(F_BLACK,BRIGHTNESS);
+  eigenVerbrauch = powerSolar;
+  if(powerBatterie>0)
+    eigenVerbrauch-=powerBatterie;
+  if(powerGrid>0)
+    eigenVerbrauch-=powerGrid;
+
+  temp = round(eigenVerbrauch/powerPerLed);
+  if(temp>0)
+  {
+    bar_led_color(F_MAGENTA,BRIGHTNESS,posMarker,temp);
+    posMarker += temp;
+  }
+
+  temp = round(powerBatterie/powerPerLed);
+  if(temp>0)
+  {
+    bar_led_color(F_TUERKIS,BRIGHTNESS,posMarker,posMarker+temp);
+    posMarker += temp;
+  }
+  else
+  {
+    bar_led_color(F_LILA,BRIGHTNESS,negMarker+temp,negMarker);  // temp ist negativ !!!
+    negMarker += temp;
+  }
+
   temp = round(powerGrid/(powerPerLed));
   if(temp>0)
   {
@@ -21,20 +47,5 @@ int8_t temp;
     bar_led_color(F_ROT,BRIGHTNESS,negMarker+temp,negMarker);  // temp ist negativ !!!
     negMarker += temp;
   }
-  temp = round(powerBatterie/powerPerLed);
-  if(temp>0)
-  {
-    bar_led_color(F_TUERKIS,BRIGHTNESS,posMarker,posMarker+temp);
-    posMarker += temp;
-  }
-  else
-  {
-    bar_led_color(F_BLAU,BRIGHTNESS,negMarker+temp,negMarker);  // temp ist negativ !!!
-    negMarker += temp;
-  }
-  temp = round(powerSolar/powerPerLed);
-  if(temp>0)
-  {
-    bar_led_color(F_GELB,BRIGHTNESS,posMarker,temp);
-  }
+
 }
